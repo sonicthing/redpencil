f0d1209976821f34b7d2f3cef70e6e253a183613
maccana <mikeymccann@gmail.com>
2013-08-19 17:44:10 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redpencil::Application.config.secret_token = '2bd737dd5b2270f8ea75d09218e5885a742c33fd8e520df00501103df4dba6e1aab9bc42a67035e00fb739743fd7fd9a71104cae48f6cea2f2c26e3851f969ae'
